6af23f5b3dd63419762a7db246c900c706b8e1cf
UnicornDad8 <unicorndad8@gmail.com>
2024-07-11 19:24:01 -0400

    Added circle with score

--- challenge/src/App.js
+++ challenge/src/App.js
@@ -7,6 +7,10 @@ function App() {
         <section className="gradient-bg result-box left">
           <div className="result-container">
             <h3>Your Result</h3>
+            <div className="circle">
+              <h2>76</h2>
+              <span>of 100</span>
+            </div>
             <h2>Great</h2>
             <p>
               You scored higher than 65% of the people who have taken these
