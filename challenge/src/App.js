c402ada79b0b387d81637e5e64be2026f5c44377
UnicornDad8 <unicorndad8@gmail.com>
2024-07-11 18:47:14 -0400

    Add some responsive styling

--- challenge/src/App.js
+++ challenge/src/App.js
@@ -3,12 +3,23 @@ import "./index.css";
 function App() {
   return (
     <main>
-      <section className="gradient-bg result-box">
-        <div className="result-container">
-          <h3>Your Result</h3>
-        </div>
-      </section>
-      <section></section>
+      <div className="main-container">
+        <section className="gradient-bg result-box left">
+          <div className="result-container">
+            <h3>Your Result</h3>
+            <h2>Great</h2>
+            <p>
+              You scored higher than 65% of the people who have taken these
+              tests.
+            </p>
+          </div>
+        </section>
+        <section className="summary right">
+          <div className="sumary-container">
+            <h3>Summary</h3>
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
