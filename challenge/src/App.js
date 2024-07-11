16da831d1dcbdacfe13f1a936dde7400a4b91826
UnicornDad8 <unicorndad8@gmail.com>
2024-07-11 17:09:11 -0400

    Added gradient to background

--- challenge/src/App.js
+++ challenge/src/App.js
@@ -1,5 +1,16 @@
+import "./index.css";
+
 function App() {
-  return <div>hola mundo</div>;
+  return (
+    <main>
+      <section className="gradient-bg result-box">
+        <div className="result-container">
+          <h3>Your Result</h3>
+        </div>
+      </section>
+      <section></section>
+    </main>
+  );
 }
 
 export default App;
